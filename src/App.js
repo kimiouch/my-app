ee83e63e12719c09eb8e73a4cf406689d8b9bd12
kimia <kimiahaghighi9877@gmail.com>
2021-12-03 00:34:55 +0330

    add react weather app to git repository

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Hello React</a>
       </header>
     </div>
   );
